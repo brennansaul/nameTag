aed6fe8bff07061267761d9640b28d909d78ed05
Brennan Saul <jamessaul7629@gmail.com>
2017-09-18 21:08:41 -0500

    upload bitmap images and nametag.py script

---
+++ nametag.py
@@ -0,0 +1,32 @@
+from papirus import Papirus
+import time
+
+# Set Screen rotation
+# Optional rotation argument: rot = 0, 90, 180 or 270
+screen = Papirus([rotation = 0])
+
+
+#Display loop
+while True:
+  # 1 Original Screen Major / Minor / Graduation displays for 15 seconds
+  # Write a bitmap to the epaper screen
+  screen.display('./')
+
+  # Update only the changed pixels (faster)
+  screen.update()
+
+  # wait for 15 seconds
+  time.sleep(.1)
+
+  # 2 Display Hobbies and Interests 10 seconds
+
+  # Write a bitmap to the epaper screen
+  screen.display('./')
+
+  # Update only the changed pixels (faster)
+  screen.partial_update()
+
+  # wait for 15 seconds
+  time.sleep(.1)
+
+  # Time Display 
