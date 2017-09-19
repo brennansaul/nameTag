a671aa02511263b39c5fb755183124451dd58e91
Brennan Saul <jamessaul7629@gmail.com>
2017-09-19 00:43:54 -0500

    working demo

--- nametag.py
+++ nametag.py
@@ -1,21 +1,34 @@
 #!/usr/bin/python
 
+import time
+import sys 
+import os
+import smbus
+
+from datetime import datetime
+
+import Image
+import ImageDraw
+import ImageFont
+
 from papirus import Papirus
+from papirus import PapirusImage
+
 import time
 
 # Set Screen rotation
 # Optional rotation argument: rot = 0, 90, 180 or 270
-screen = Papirus([rotation = 0])
-
+#screen = Papirus(0)
+image = PapirusImage(180)
 
 #Display loop
-#while True:
+while True:
   # 1 Original Screen Major / Minor / Graduation displays for 15 seconds
   # Write a bitmap to the epaper screen
-  screen.display('./nametagmajorbox.png')
+  image.write('./nametagmajorbox.png')
 
   # Update only the changed pixels (faster)
-  screen.update()
+  #screen.update()
 
   # wait for 15 seconds
   time.sleep(10)
@@ -23,12 +36,12 @@ screen = Papirus([rotation = 0])
   # 2 Display Hobbies and Interests 10 seconds
 
   # Write a bitmap to the epaper screen
-  screen.display('./factsnametagbox.png')
+  image.write('./factsnametagbox.png')
 
   # Update only the changed pixels (faster)
-  screen.partial_update()
+  #screen.partial_update()
 
   # wait for 15 seconds
-  time.sleep(1)
+  time.sleep(10)
 
   # Time Display 
