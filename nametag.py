70ed0ad7f6df74f74206a5dbac90019a531b15c4
Brennan Saul <jamessaul7629@gmail.com>
2017-09-20 12:03:42 -0500

    Ready for job fair

--- nametag.py
+++ nametag.py
@@ -26,34 +26,27 @@ while True:
   image.write('./nametagmajorbox.png')
 
   # Wait / display for 15 seconds
-  #time.sleep(15)
-  time.sleep(3) # For dev
+  time.sleep(15)
+  #time.sleep(3) # For dev
 
-  # 2 Display Hobbies and Interests 10 seconds
+  # 2 Display Hobbies and Interests 15 seconds
   # Write image to the epaper screen
   image.write('./factsnametagbox.png')
 
-  # Wait / display for 15 seconds
-  #time.sleep(15)
-  time.sleep(3) # For Dev
-  # Time Display 
-  #t = datetime.datetime.now()
-  #timeString = t.strftime("%H:%M")
-  #image.write('./Basenametagbox.png')
-  #text.AddText(timeString, 150, 80, Id="Start")
+  # Wait / display for 10 seconds
+  time.sleep(10)
+  #time.sleep(3) # For Dev
   
+  # 3 Display Time and Date 10 seconds 
   # Calling PapirusComposite this way will mean nothing is written to the screen until WriteAll is called
   textNImg = PapirusComposite(False, 180)
   
-  # String storing time data
+  # String storing time and data into respective strings
   t = datetime.datetime.now()
   timeString = t.strftime("%I:%M %p")
   dateString = t.strftime("%a %b %d")
-  # Write text to the screen at selected point, with an Id
-  # Nothing will show on the screen
-  #textNImg.AddText(timeString, 80, 30, Id="time")
-  #textNImg.AddText(dateString, 80, 25, Id="date")
-  # Add image with default layout
+
+  # Add image with default layout date text and time text
   textNImg.AddImg("./Basenametagbox.png", 0, 0, (200, 96), Id="NameLogo")
   textNImg.AddText(dateString, 65, 20, Id="date")
   textNImg.AddText(timeString, 65, 40, Id="time")
@@ -61,4 +54,5 @@ while True:
   # Now display all elements on the scrren
   textNImg.WriteAll()
   
-  time.sleep(3)
+  time.sleep(10)
+  #time.sleep(3) For Dev
